4e8c2bcdd76b806031fce9146ad9cd0d98a36920
Vadik365 <vadiksoldatenko@gmail.com>
2026-03-05 18:33:34 +0200

    Implement CRUD endpoints for goals and tasks

--- planner/urls.py
+++ planner/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import GoalListCreateView, TaskListCreateView
+from .views import GoalListCreateView, GoalDetailView, TaskListCreateView, TaskDetailView
 
 urlpatterns = [
     path('goals/', GoalListCreateView.as_view()),
+    path('goals/<int:pk>/', GoalListCreateView.as_view()),
+
     path('tasks/', TaskListCreateView.as_view()),
+    path('tasks/<int:pk>/', TaskDetailView.as_view()),
 ]
--- planner/views.py
+++ planner/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.exceptions import PermissionDenied
 from rest_framework import generics
 from .models import Goal,Task
 from .serializers import GoalSerializer, TaskSerializer
@@ -21,9 +22,24 @@ class TaskListCreateView(generics.ListCreateAPIView):
         # Then we filter tasks through relationship:
         # Task → Goal → User
         return Task.objects.filter(goal__user=self.request.user)
+    def perform_create(self, serializer):
+        # Get Goal object from validated data
+        goal = serializer.validated_data["goal"]
+        #Security chek: goal must belong to cuurent user
+        if goal.user != self.request.user:
+            raise PermissionDenied("You cannot create tasks for other users")
+        #Save task if everything is Ok
+        serializer.save()
     serializer_class = TaskSerializer
 
+class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TaskSerializer
+    def get_queryset(self):
+        return Task.objects.filter(goal__user=self.request.user)
 
-
+class GoalDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = GoalSerializer
+    def get_queryset(self):
+        return Goal.objects.filter(goal__user=self.request.user)
 
 
